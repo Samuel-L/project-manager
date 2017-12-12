6ae05ee8ed753ba06097f6b792edd6d96b08d06e
Samuel-L <samuell.person@gmail.com>
2017-12-12 21:20:41 +0000

    fix(utils): change obj.user to obj.owner to

--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -9,4 +9,4 @@ class IsOwner(permissions.BasePermission):
         return request.user and request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
+        return obj.owner == request.user
