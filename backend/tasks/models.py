9b46c5d3b1d629fc575cdd05d517ce5bc36de68a
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:28:44 +0000

    add(tasks): scope model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -10,10 +10,21 @@ class Project(models.Model):
     project_description = models.TextField()
 
 
+class Scope(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    scope_name = models.CharField(max_length=255)
+    scope_description = models.TextField()
+    priority = models.IntegerField(choices=PRIORITIES)
+    due_date = models.DateTimeField()
+
+
 class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     project = models.ForeignKey(Project, blank=True, null=True,
                                 on_delete=models.CASCADE)
+    scope = models.ForeignKey(Scope, blank=True, null=True,
+                              on_delete=models.CASCADE)
     task_name = models.CharField(max_length=255)
     task_description = models.TextField()
     priority = models.IntegerField(choices=PRIORITIES)
