a5b91cd0349d7f033e13ed7d095d6c86317e44fc
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:28:53 +0000

    add(tasks): scope serializer

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from tasks.models import Task, Project
+from tasks.models import Task, Project, Scope
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -9,6 +9,15 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'owner', 'project_name', 'project_description')
+
+
+class ScopeSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Scope
+        fields = ('id', 'owner', 'project', 'scope_name', 'scope_description',
+                  'priority', 'due_date')
 
 
 class TaskSerializer(serializers.ModelSerializer):
