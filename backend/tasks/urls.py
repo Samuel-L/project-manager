58a2aa40f0f76def189effbd5cc5d92ffbc11b39
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:14:37 +0000

    add(tasks): project urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,9 +1,10 @@
 from rest_framework import routers
 
-from tasks.views import TaskViewSet
+from tasks.views import TaskViewSet, ProjectViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'tasks/tasks', TaskViewSet, 'tasks')
+router.register(r'tasks/projects', ProjectViewSet, 'projects')
 
 urlpatterns = router.urls
