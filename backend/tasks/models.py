434ca531a6382c9092385bb3f2a8e212e3860f12
Samuel-L <samuell.person@gmail.com>
2017-12-12 19:28:17 +0000

    add(tasks): created model for task

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from tasks.consts import PRIORITIES
+
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_name = models.CharField(max_length=255)
+    task_description = models.TextField()
+    priority = models.IntegerField(choices=PRIORITIES)
+    due_date = models.DateTimeField()
