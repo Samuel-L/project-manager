c14de1a054952d0e526847cee38b2eed41ba314b
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:08:30 +0000

    add(tasks): project model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import User
 from tasks.consts import PRIORITIES
 
 
+class Project(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=255)
+    project_description = models.TextField()
+
+
 class Task(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, blank=True, null=True,
+                                on_delete=models.CASCADE)
     task_name = models.CharField(max_length=255)
     task_description = models.TextField()
     priority = models.IntegerField(choices=PRIORITIES)
