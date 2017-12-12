932c91ae1f505ff7faebb4f742b6d6e69f753975
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:10:13 +0000

    add(tasks): project serializer

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -3,6 +3,14 @@ from rest_framework import serializers
 from tasks.models import Task
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Task
+        fields = ('id', 'owner', 'project_name', 'project_description')
+
+
 class TaskSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
 
