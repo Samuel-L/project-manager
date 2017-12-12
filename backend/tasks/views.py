4fd7277bc0a1ca84c18eb3de5131a9d31215aa3c
Samuel-L <samuell.person@gmail.com>
2017-12-12 19:41:43 +0000

    add(tasks/utils): viewset for Task model and permission IsOwner

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+from utils.permissions import IsOwner
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    serializer_class = TaskSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        """Only return tasks that user created."""
+        tasks = Task.object.filter(owner=self.request.user)
+        return tasks
+
+    def perform_create(self, serializer):
+        """Set owner to the user creating the task and create entry."""
+        serializer.save(owner=self.request.user)
