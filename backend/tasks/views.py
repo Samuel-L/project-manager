f4b622b47acbc0d318715c64f350ed0756440638
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:13:16 +0000

    add(tasks): project viewset

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,8 +1,22 @@
 from rest_framework import viewsets
 
-from tasks.models import Task
-from tasks.serializers import TaskSerializer
+from tasks.models import Task, Project
+from tasks.serializers import TaskSerializer, ProjectSerializer
 from utils.permissions import IsOwner
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    serializer_class = ProjectSerializer
+    permission_classes = (IsOwner,)
+
+    def get_queryset(self):
+        """Only return projects that user created."""
+        projects = Project.object.filter(owner=self.request.user)
+        return projects
+
+    def perform_create(self, serializer):
+        """Set owner to the user creating the project and create entry."""
+        serializer.save(owner=self.request.user)
 
 
 class TaskViewSet(viewsets.ModelViewSet):
