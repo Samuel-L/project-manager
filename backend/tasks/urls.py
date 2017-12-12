9f8b54cecd52081bad6d60e2b237d1546d391afe
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:29:10 +0000

    add(tasks): scope urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,10 +1,11 @@
 from rest_framework import routers
 
-from tasks.views import TaskViewSet, ProjectViewSet
+from tasks.views import TaskViewSet, ProjectViewSet, ScopeViewSet
 
 
 router = routers.SimpleRouter()
 router.register(r'tasks/tasks', TaskViewSet, 'tasks')
 router.register(r'tasks/projects', ProjectViewSet, 'projects')
+router.register(r'tasks/scopes', ScopeViewSet, 'scopes')
 
 urlpatterns = router.urls
