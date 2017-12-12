27968995f815c06741dbf273989073fe43712291
Samuel-L <samuell.person@gmail.com>
2017-12-12 20:01:28 +0000

    refact(utils): change IsOwner to return false if user is anonymous

--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -3,9 +3,11 @@ from rest_framework import permissions
 
 class IsOwner(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it.
+    Custom permission that only allows owner to edit and view tasks
     """
     def has_permission(self, request, view):
+        if request.user.is_anonymous:
+            return False
         return request.user and request.user.is_authenticated()
 
     def has_object_permission(self, request, view, obj):
