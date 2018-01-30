b0bc4d8fd2de02346c7898be56272a6af72a257c
Samuel-L <samuell.person@gmail.com>
2018-01-30 15:48:34 +0000

    add(frontend): barebones dashboard

--- client/src/scenes/Dashboard/index.jsx
+++ client/src/scenes/Dashboard/index.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link, Route } from 'react-router-dom';
 
 export default class Dashboard extends React.Component {
   render() {
     return (
-      <h1>Lgged in</h1>
+      <div className="app-container">
+        <div className="nav-bar">
+          <span className="username-display">Username</span>
+          <ul className="nav-links">
+            <li className="link">
+              <Link to="/">Overview</Link>
+            </li>
+            <li className="link">
+              <Link to="/projects">Projects</Link>
+            </li>
+            <li className="link">
+              <Link to="/reports">Reports</Link>
+            </li>
+            <li className="link">
+              <Link to="/calendar">Calendar</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="content">
+          <Route exact path="/" />
+        </div>
+      </div>
     );
   }
 }
