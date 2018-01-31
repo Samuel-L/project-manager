81318f18e085b23d767e5d8b054df22b48f076ee
Samuel L <samuell.person@gmail.com>
2018-01-31 22:28:14 +0100

    change(frontend:auth-nav): navigation bar is now dynamic

--- client/src/components/NavBars/AuthNav/index.jsx
+++ client/src/components/NavBars/AuthNav/index.jsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 
-export default class NotAuthNav extends React.Component {
+const Nav = withRouter(props => <NotAuthNav {...props} />);
+
+class NotAuthNav extends React.Component {
   render() {
+    // eslint-disable-next-line react/prop-types
+    this.pathname = this.props.location.pathname;
+
     return (
       <div className="auth-nav">
         <h1 className="username-display">Username</h1>
         <ul className="nav-links">
-          <li className="link link-selected">
+          <li
+            className={this.pathname === '/' ? 'link link-selected' : 'link'}
+          >
             <Link to="/">Overview</Link>
           </li>
-          <li className="link">
-            <Link to="/login">Login</Link>
+          <li
+            className={this.pathname === '/projects' ? 'link link-selected' : 'link'}
+          >
+            <Link to="/projects">Projects</Link>
           </li>
-          <li className="link">
-            <Link to="/">Home</Link>
+          <li
+            className={this.pathname === '/reports' ? 'link link-selected' : 'link'}
+          >
+            <Link to="/reports">Reports</Link>
           </li>
         </ul>
       </div>
     );
   }
 }
+
+export default Nav;
