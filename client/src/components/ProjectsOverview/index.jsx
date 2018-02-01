2aea8f346cbc79336c737c4202aaecd7c1e9f195
Samuel L <samuell.person@gmail.com>
2018-02-01 22:18:18 +0100

    add(frontend:projects-overview): added a notice if there are no projects

--- client/src/components/ProjectsOverview/index.jsx
+++ client/src/components/ProjectsOverview/index.jsx
@@ -17,20 +17,25 @@ export default class ProjectsOverview extends React.Component {
 
   projectItems() {
     let projects;
-    if (this.props.projects) {
+    let projectItems;
+    if (this.props.projects !== '[]' && this.props.projects !== null) {
       projects = JSON.parse(this.props.projects);
-    } else { projects = []; }
-
-    const projectItems = projects.map(proj =>
-      (
-        <Project
-          desc={proj.project_description}
-          tasks={proj.total_tasks}
-          finishedTasks={proj.total_finished_tasks}
-        >
-          { proj.project_name }
-        </Project>
-      ));
+      projectItems = projects.map(proj =>
+        (
+          <Project
+            desc={proj.project_description}
+            tasks={proj.total_tasks}
+            finishedTasks={proj.total_finished_tasks}
+          >
+            { proj.project_name }
+          </Project>
+        ));
+    } else {
+      projectItems = (
+        <li className="no-projects-notice">
+          It seems like you have no projects!
+        </li>);
+    }
 
     return projectItems;
   }
