5778313e724dcd47c5b87491f07ad3b1221d460e
Samuel L <samuell.person@gmail.com>
2018-02-04 01:23:18 +0100

    add(frontend:tasks-overview): base created for tasks overview. no functionality yet

--- client/src/components/TasksOverview/index.jsx
+++ client/src/components/TasksOverview/index.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 
 import OverviewHeading from '../../elements/OverviewHeading/index';
+import Task from '../../elements/Task/index';
 
 export default class TasksOverview extends React.Component {
   render() {
     return (
       <div className="tasks-container">
         <OverviewHeading>Tasks</OverviewHeading>
+        <ul className="tasks">
+          <Task
+            project="Test Project"
+            priority="NOT IMPORTANT"
+            dueDate="2017/01/01"
+          >
+            Task 1
+          </Task>
+          <Task
+            project="Test Project"
+            priority="NOT IMPORTANT"
+            dueDate="2017/01/01"
+          >
+            Task 1
+          </Task>
+          <Task
+            project="Test Project"
+            priority="NOT IMPORTANT"
+            dueDate="2017/01/01"
+          >
+            Task 1
+          </Task>
+          <Task
+            project="Test Project"
+            priority="NOT IMPORTANT"
+            dueDate="2017/01/01"
+          >
+            Task 1
+          </Task>
+        </ul>
       </div>
     );
   }
