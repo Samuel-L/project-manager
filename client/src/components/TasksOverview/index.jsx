c183f456efd21b162cc6e619a2819ad797581352
Samuel L <samuell.person@gmail.com>
2018-02-04 01:56:41 +0100

    add(frontend:tasks-overview): add data retrieval for component

--- client/src/components/TasksOverview/index.jsx
+++ client/src/components/TasksOverview/index.jsx
@@ -1,42 +1,52 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import OverviewHeading from '../../elements/OverviewHeading/index';
 import Task from '../../elements/Task/index';
 
+import fetchTasks from '../../actions/task';
+
+@connect(store => ({
+  tasks: store.task.tasks,
+}))
 export default class TasksOverview extends React.Component {
+  componentWillMount() {
+    this.props.dispatch(fetchTasks()); // eslint-disable-line react/prop-types
+  }
+
+  taskItems() {
+    let tasks;
+    let taskItems;
+    console.log(this.props)
+    if (this.props.tasks !== '[]' && this.props.tasks !== null) {
+      tasks = JSON.parse(this.props.tasks);
+      taskItems = tasks.map(task =>
+        (
+          <Task
+            project={task.project}
+            priority={task.priority}
+            dueDate={task.due_date}
+          >
+            { task.task_name }
+          </Task>
+        ));
+    } else {
+      taskItems = (
+        <li className="no-tasks-notice">
+          It seems like you have no tasks!
+        </li>);
+    }
+
+    return taskItems;
+  }
+
   render() {
     return (
       <div className="tasks-container">
         <OverviewHeading>Tasks</OverviewHeading>
         <ul className="tasks">
-          <Task
-            project="Test Project"
-            priority="NOT IMPORTANT"
-            dueDate="2017/01/01"
-          >
-            Task 1
-          </Task>
-          <Task
-            project="Test Project"
-            priority="NOT IMPORTANT"
-            dueDate="2017/01/01"
-          >
-            Task 1
-          </Task>
-          <Task
-            project="Test Project"
-            priority="NOT IMPORTANT"
-            dueDate="2017/01/01"
-          >
-            Task 1
-          </Task>
-          <Task
-            project="Test Project"
-            priority="NOT IMPORTANT"
-            dueDate="2017/01/01"
-          >
-            Task 1
-          </Task>
+          { this.taskItems() }
         </ul>
       </div>
     );
