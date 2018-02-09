651befea56d37ae48ce8d68ac9ad87962278e275
Samuel L <samuell.person@gmail.com>
2018-02-09 22:21:50 +0100

    remove(time-worked-overview): abandoning chart idea. files removed.

--- client/src/components/TimeWorkedOverview/index.jsx
+++ client/src/components/TimeWorkedOverview/index.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import OverviewHeading from '../../elements/OverviewHeading/index';
-import LineChart from '../../elements/LineChart/index';
 
 import fetchReports from '../../actions/report';
 
@@ -20,16 +18,7 @@ export default class TimeWorkedOverview extends React.Component {
     return (
       <div className="time-worked-container">
         <OverviewHeading>Time Worked</OverviewHeading>
-        <LineChart reports={this.props.reports} />
       </div>
     );
   }
 }
-
-TimeWorkedOverview.propTypes = {
-  reports: PropTypes.string,
-};
-
-TimeWorkedOverview.defaultProps = {
-  reports: '',
-};
