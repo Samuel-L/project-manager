e4ca6f6e08ba8289c9ed1ba4add42890a7f2d6fc
Samuel L <samuell.person@gmail.com>
2018-02-10 22:33:10 +0100

    refact(frontend:OverviewHeading): refactored into functional component

--- client/src/elements/OverviewHeading/index.jsx
+++ client/src/elements/OverviewHeading/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class OverviewHeading extends React.Component {
-  render() {
-    return (
-      <h2 className="overview-heading">{this.props.children}</h2>
-    );
-  }
-}
+const OverviewHeading = props => (
+  <h2 className="overview-heading">{props.children}</h2>
+);
 
 OverviewHeading.propTypes = {
   children: PropTypes.string.isRequired,
