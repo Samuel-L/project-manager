970981ea6662b91554b970be6486cdda6a3670aa
Samuel L <samuell.person@gmail.com>
2018-02-10 19:09:36 +0100

    fix(frontend:time-worked-overview): eslint errors

--- client/src/components/TimeWorkedOverview/index.jsx
+++ client/src/components/TimeWorkedOverview/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 import OverviewHeading from '../../elements/OverviewHeading/index';
@@ -17,7 +18,6 @@ export default class TimeWorkedOverview extends React.Component {
   reportItems() {
     let projectItems;
     if (this.props.projects !== '[]' && this.props.projects !== null) {
-      console.log(this.props.reports);
       const projects = JSON.parse(this.props.projects);
       projectItems = projects.map(project => (
         <Report
@@ -48,3 +48,11 @@ export default class TimeWorkedOverview extends React.Component {
     );
   }
 }
+
+TimeWorkedOverview.propTypes = {
+  projects: PropTypes.string,
+};
+
+TimeWorkedOverview.defaultProps = {
+  projects: '',
+};
