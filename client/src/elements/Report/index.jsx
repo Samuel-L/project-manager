3718cbbe8ab959883056faf7f76dd0725aff221b
Samuel L <samuell.person@gmail.com>
2018-02-10 22:36:09 +0100

    refact(frontend:Report): refactored into functional component

--- client/src/elements/Report/index.jsx
+++ client/src/elements/Report/index.jsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Report extends React.Component {
-  render() {
-    return (
-      <li className="report">
-        <h3 className="report__heading">{this.props.children}</h3>
-        <div className="report__info-container">
-          <p className="report__info">
-            <span className="report__label">Last 7 days:</span> {this.props.lastSevenDays}
-          </p>
-          <p className="report__info">
-            <span className="report__label">Total:</span> {this.props.total}
-          </p>
-        </div>
-      </li>
-    );
-  }
-}
+const Report = props => (
+  <li className="report">
+    <h3 className="report__heading">{props.children}</h3>
+    <div className="report__info-container">
+      <p className="report__info">
+        <span className="report__label">Last 7 days:</span> {props.lastSevenDays}
+      </p>
+      <p className="report__info">
+        <span className="report__label">Total:</span> {props.total}
+      </p>
+    </div>
+  </li>
+);
 
 Report.propTypes = {
   children: PropTypes.string.isRequired,
   lastSevenDays: PropTypes.string.isRequired,
   total: PropTypes.string.isRequired,
 };
+
+export default Report;
