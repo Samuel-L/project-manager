62d29ab2b8f227663b944ed60f8fe812b49131ea
Samuel L <samuell.person@gmail.com>
2018-02-10 19:34:21 +0100

    fix(backend): correct data is now required when sending POST request

--- backend/reports/serializers.py
+++ backend/reports/serializers.py
@@ -7,6 +7,14 @@ from reports.models import Report
 
 class ReportSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Report
+        fields = ('id', 'owner', 'task', 'project', 'scope', 'start', 'end',
+                  'total_in_seconds')
+
+class DetailedReportSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
     project = ProjectSerializer()
 
     class Meta:
--- backend/reports/views.py
+++ backend/reports/views.py
@@ -1,13 +1,20 @@
 from rest_framework import viewsets
 
 from reports.models import Report
-from reports.serializers import ReportSerializer
+from reports.serializers import ReportSerializer, DetailedReportSerializer
 from utils.permissions import IsOwner
 
 
 class ReportViewSet(viewsets.ModelViewSet):
     serializer_class = ReportSerializer
     permission_classes = (IsOwner,)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return DetailedReportSerializer
+        if self.action == 'retrieve':
+            return DetailedReportSerializer
+        return ReportSerializer
 
     def get_queryset(self):
         """Only return reports that user created."""
