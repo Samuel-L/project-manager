e0e6bd7106a75a33c66fd47edc439f9b97f49514
Samuel L <samuell.person@gmail.com>
2018-02-10 22:32:07 +0100

    refact(frontend:PageHeading): refactored into functional component

--- client/src/elements/PageHeading/index.jsx
+++ client/src/elements/PageHeading/index.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class PageHeading extends React.Component {
-  render() {
-    return (
-      <h1 className="page-heading">{this.props.children}</h1>
-    );
-  }
-}
+const PageHeading = props => (
+  <h1 className="page-heading">{props.children}</h1>
+);
 
 PageHeading.propTypes = {
   children: PropTypes.string.isRequired,
 };
+
+export default PageHeading;
